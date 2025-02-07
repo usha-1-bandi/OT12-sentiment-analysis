d28fff8de3240b0e532ac6e4f82f53d2ed651ec1
usha <ushasribandi44@gmail.com>
2025-02-07 10:30:23 +0530

    enter

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route("/")
-def Home():
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route("/predict")
+def home():
     return render_template("index.html")
-if__name__="__main__":
-    app.run()
+@app.route("/predict",methods=["GET","POST"])
+def predict():
+    if request.method=="POST":
+        msg=request.form.get("message")
+        print(msg)
+    else:
+        return render_template("/predict.html")
+        
+   # return render_template("predict.html")
+if __name__ == "__main__":
+    app.run(debug=True)
