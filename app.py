4fc6a8bee6102865accd5ef6889d7e4b49db7495
usha <ushasribandi44@gmail.com>
2025-02-08 12:36:10 +0530

    yes

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template,request
-
-app = Flask(__name__)
-
-@app.route("/predict")
-def home():
+from flask import Flask,render_template, request
+app=Flask(_name_)
+@app.route("/")
+def Home():
     return render_template("index.html")
 @app.route("/predict",methods=["GET","POST"])
 def predict():
-    if request.method=="POST":
-        msg=request.form.get("message")
+    if request.method == "POST":
+        msg =request.form.get("message")
         print(msg)
     else:
-        return render_template("/predict.html")
-        
-   # return render_template("predict.html")
-if __name__ == "__main__":
-    app.run(debug=True)
+        return render_template("predict.html")
+
+if _name_=="__main__":
+    app.run(host="0.0.0.0",port=5050)
+
+
+
+
+
+
+
+
+
+
+
+
+    
